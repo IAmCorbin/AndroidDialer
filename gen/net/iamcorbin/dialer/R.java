d3d7f69329f2ef3844eadbae4107c739e4b0a4e3
Corbin Tarrant <Corbin@IAmCorbin.net>
2010-07-17 15:21:23 -0400

    Finished adding new graphic

--- gen/net/iamcorbin/dialer/R.java
+++ gen/net/iamcorbin/dialer/R.java
@@ -47,13 +47,16 @@ public final class R {
         public static final int contactNameTextView=0x7f080000;
         public static final int contactsListView=0x7f080001;
         public static final int displayNumber=0x7f080002;
-        public static final int pick_contact=0x7f080011;
+        public static final int numbersListView=0x7f080012;
+        public static final int pick_contact=0x7f080013;
+        public static final int pick_number_name=0x7f080011;
     }
     public static final class layout {
         public static final int contact=0x7f030000;
         public static final int contact_picker=0x7f030001;
         public static final int dialpad=0x7f030002;
         public static final int main=0x7f030003;
+        public static final int number_picker=0x7f030004;
     }
     public static final class menu {
         public static final int menu=0x7f070000;
