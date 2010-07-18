2e4f8ad5165ad6622cf8f14f80021bda24d4dd9f
Corbin Tarrant <Corbin@IAmCorbin.net>
2010-07-18 03:40:33 -0400

    Added Simple Preferences

--- src/net/iamcorbin/dialer/Dialer.java
+++ src/net/iamcorbin/dialer/Dialer.java
@@ -4,9 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,20 +18,28 @@ import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Dialer extends Activity implements OnClickListener {
+	//Intent IDs
 	private static final int PICK_CONTACT = 1;
 	private static final int DIAL_NUMBER = 2;
+	private static final int USER_PREFS = 3;
+
+	//Debug tag
 	private static final String TAG = "Dialer";
 	
 	//Preferences
 	private static final String PREF_NUMBER = "PREF_NUMBER";
+	private static final String PREF_BUTTON_TYPE = "PREF_BUTTON_TYPE";
 	
+	//Components
+	private Button buttonClear;
 	private Button buttonDial;
 	private TextView number;
+	LinearLayout buttons[] = new LinearLayout[12];
 	
     /** Called when the activity is first created. */
     @Override
@@ -38,6 +50,19 @@ public class Dialer extends Activity implements OnClickListener {
         
         setContentView(R.layout.main);
         
+        this.buttons[0] = (LinearLayout)findViewById(R.id.button0);
+		this.buttons[1] = (LinearLayout)findViewById(R.id.button1);
+		this.buttons[2] = (LinearLayout)findViewById(R.id.button2);
+		this.buttons[3] = (LinearLayout)findViewById(R.id.button3);
+		this.buttons[4] = (LinearLayout)findViewById(R.id.button4);
+		this.buttons[5] = (LinearLayout)findViewById(R.id.button5);
+		this.buttons[6] = (LinearLayout)findViewById(R.id.button6);
+		this.buttons[7] = (LinearLayout)findViewById(R.id.button7);
+		this.buttons[8] = (LinearLayout)findViewById(R.id.button8);
+		this.buttons[9] = (LinearLayout)findViewById(R.id.button9);
+		this.buttons[10] = (LinearLayout)findViewById(R.id.buttonStar);
+		this.buttons[11] = (LinearLayout)findViewById(R.id.buttonPound);
+        this.buttonClear = (Button)findViewById(R.id.buttonClearNumber);
         this.buttonDial = (Button)findViewById(R.id.buttonDial);
         this.number = (TextView)findViewById(R.id.displayNumber);
                 
@@ -56,12 +81,16 @@ public class Dialer extends Activity implements OnClickListener {
     	super.onOptionsItemSelected(item);
     	switch(item.getItemId()) {
     		case R.id.pick_contact:
-    			Intent intent = new Intent(Dialer.this, ContactPicker.class);
-    			startActivityForResult(intent, PICK_CONTACT);
+    			Intent intent_contact = new Intent(Dialer.this, ContactPicker.class);
+    			startActivityForResult(intent_contact, PICK_CONTACT);
+    			return true;
+    		case R.id.user_prefs:
+    			Intent intent_prefs = new Intent(Dialer.this, UserPreferences.class);
+    			startActivityForResult(intent_prefs, USER_PREFS);
     			return true;
     	}
     	
-    	return true;
+    	return false;
     }
     
     public void onClick(View v) {
@@ -99,7 +128,6 @@ public class Dialer extends Activity implements OnClickListener {
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
     	savedInstanceState.putString(PREF_NUMBER, this.number.getText().toString());
-    	
     	super.onSaveInstanceState(savedInstanceState);    	
     }
     
@@ -107,5 +135,22 @@ public class Dialer extends Activity implements OnClickListener {
     public void onRestoreInstanceState(Bundle savedInstanceState) {
     	if(savedInstanceState != null && savedInstanceState.containsKey(PREF_NUMBER))
     		this.number.setText(savedInstanceState.getString(PREF_NUMBER));
+    	
+    	Context context = getApplicationContext();
+    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);    	
+    	String button_type = prefs.getString(PREF_BUTTON_TYPE, "button_green");
+    	if(button_type.equals("button_green")) {
+    			for(LinearLayout button : this.buttons) {
+    				button.setBackgroundResource(R.drawable.button_green);  
+    			}
+    			this.buttonDial.setBackgroundResource(R.drawable.button_green);
+    			this.buttonClear.setBackgroundResource(R.drawable.button_green);
+    	} else if(button_type.equals("button_smoke")) {
+    		for(LinearLayout button : this.buttons) {
+				button.setBackgroundResource(R.drawable.button_smoke);  
+    		}
+			this.buttonDial.setBackgroundResource(R.drawable.button_smoke);
+			this.buttonClear.setBackgroundResource(R.drawable.button_smoke);
+    	}
     }
 }
